1bc4a710ecf3a4c373b939ab070a71e91d789246
chanjin <chanjin5212@gmail.com>
2025-12-06 17:14:51 +0900

    커밋

--- js/tower-click.js
+++ js/tower-click.js
@@ -1,4 +1,4 @@
-// 타워 클릭 핸들러 (좌표 수정 버전)
+// 타워 클릭 핸들러 (칸 선택만)
 
 (function () {
     function initTowerClick() {
@@ -21,19 +21,7 @@
             const x = (event.clientX - rect.left) * scaleX;
             const y = (event.clientY - rect.top) * scaleY;
 
-            console.log('클릭 좌표:', x, y);
-
-            // 먼저 타워 클릭 체크
-            const tower = this.towerManager.getTowerAt(x, y);
-            if (tower) {
-                this.towerManager.selectTower(tower);
-                if (typeof showTowerDetails === 'function') {
-                    showTowerDetails(tower);
-                }
-                return;
-            }
-
-            // 그리드 영역 클릭
+            // 그리드 영역 클릭만 처리 (타워 개별 클릭 제거)
             const grid = CONFIG.GRID_AREA;
             if (x >= grid.x && x <= grid.x + grid.width &&
                 y >= grid.y && y <= grid.y + grid.height) {
@@ -41,11 +29,7 @@
                 const gridX = Math.floor((x - grid.x) / grid.cellWidth);
                 const gridY = Math.floor((y - grid.y) / grid.cellHeight);
 
-                console.log('그리드 좌표:', gridX, gridY);
-
-                if (this.towerManager.selectCell(gridX, gridY)) {
-                    // 토스트 제거 - 패널 전환으로 충분
-                }
+                this.towerManager.selectCell(gridX, gridY);
             }
         };
 
